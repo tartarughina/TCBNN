427bbd86472cb77610014f35a36ddb3f69b5c68b
Riccardo Strina <riccardo.strina@icloud.com>
2024-10-04 12:55:00 +0200

    Fix path typo in python script

--- train_data.py
+++ train_data.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    alcf_path = '/eagle/datasets/ImageNet/ILSVRC/Data/CLS-LOC/train>/'
+    alcf_path = '/eagle/datasets/ImageNet/ILSVRC/Data/CLS-LOC/train/'
     with open('imagenet_files.txt') as f:
         lines = f.readlines()
 
